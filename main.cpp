1ae0c4f48d0bd7eedba88f74c066aed29674a3fa
Kholoodkhaled <Kholoodelrefai@gmail.com>
2024-11-26 21:47:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
-
+#include "LinkedList.h"
 using namespace std;
 
 int main()
-{   int nodeNum;
+{
+    int nodeNum;
     LinkedList mylist;
 
     mylist.add(3);
     mylist.add(5);
     mylist.display();
 
-    // insert node after given node
-      InsertAfter(10, 13);
-      mylist.display();
-    // insert node before given node
-	 InsertBefore( 7, 8);
-	 mylist.display();
+    /// Insert node after a given node
+    mylist.InsertAfter(10, 3);
+    mylist.display();
+
+    /// Insert node before a given node
+    mylist.InsertBefore(7, 5);
+    mylist.display();
 
     nodeNum = mylist.GetCount();
+    cout << "\n Number of nodes: " << nodeNum << endl;
 
-
-    if(!mylist.search_using_index(10)){
-        cout<<"Not Found"<<endl;
-    }else{
-        cout<<"Found"<<endl;
+    if (!mylist.search_using_index(3))
+    {
+        cout << "Not Found" << endl;
     }
-
-
+    else
+    {
+        cout << "\n index Found" << endl;
+        cout << " Data in node index 3 is: "  <<mylist.getNodeDataByIndex(3)<< endl;;
+    }
 
     return 0;
 }
